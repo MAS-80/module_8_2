7b713871e74b6201b43ce166ae3e479b53ce545a
MAS-80 <andrej.makruschin@yandex.ru>
2024-08-14 21:20:49 +0300

    Add files via upload

---
+++ module_8_2.py
@@ -0,0 +1,28 @@
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+    for i in numbers:
+        try:
+            result += i
+        except TypeError:
+            print(f'Некорректный ввод данных для подсчета суммы-{i}')
+            incorrect_data += 1
+    return result, incorrect_data
+
+
+def calculate_average(numbers):
+    if isinstance(numbers, int):
+        return None
+    try:
+        a = personal_sum(numbers)
+        return a[0] / (len(numbers) - a[1])
+    except ZeroDivisionError:
+        return 0
+    except TypeError:
+        return f'В numbers записан некорректный тип данных'
+
+
+print(f'Результат 1:{calculate_average("1, 2, 3")}')
+print(f'Результат 2:{calculate_average([1, "Строка", 3, "Еще строка"])}')
+print(f'Результат 3:{calculate_average(567)}')
+print(f'Результат 4:{calculate_average([42, 15, 36, 13])}')
